20a06d6109b784eadf918ff1080a4d02c460a3f0
"Aloy" <"aloycwl@gmail.com">
2022-04-21 17:32:38 +0800

    erc721ac od & deposit

--- WHCC_ERC721AC.sol
+++ WHCC_ERC721AC.sol
@@ -1,7 +1,7 @@
-pragma solidity^0.8.13;//SPDX-License-Identifier: MIT
+pragma solidity^0.8.13;//SPDX-License-Identifier:None
 interface IERC721{event Transfer(address indexed from,address indexed to,uint256 indexed tokenId);event Approval(address indexed owner,address indexed approved,uint256 indexed tokenId);event ApprovalForAll(address indexed owner,address indexed operator,bool approved);function balanceOf(address owner)external view returns(uint256 balance);function ownerOf(uint256 tokenId)external view returns(address owner);function safeTransferFrom(address from,address to,uint256 tokenId)external;function transferFrom(address from,address to,uint256 tokenId)external;function approve(address to,uint256 tokenId)external;function getApproved(uint256 tokenId)external view returns(address operator);function setApprovalForAll(address operator,bool _approved)external;function isApprovedForAll(address owner,address operator)external view returns(bool);function safeTransferFrom(address from,address to,uint256 tokenId,bytes calldata data)external;}
 interface IERC721Metadata{function name()external view returns(string memory);function symbol()external view returns(string memory);function tokenURI(uint256 tokenId)external view returns(string memory);}
-interface IOwlWarLand{function BURN(address _t,uint256 _a)external;} 
+interface IOwlWarLand{function BURN(address _t,uint256 _a)external;}
 contract ERC721AC is IERC721,IERC721Metadata{
     address private _owner;
     mapping(uint256=>address)private _tokenApprovals;
@@ -122,6 +122,7 @@ contract ERC721AC is IERC721,IERC721Metadata{
     function GENPREP(uint256 k, uint256 m)external onlyOwner{
         gen[k].maxCount=m;
     }
+    function DEPOSIT()external payable{}
     function DISTRIBUTE()external{unchecked{
         bool s;
         (s,)=payable(payable(_owner)).call{value:address(this).balance/gen[1].currentCount<168?95:5*100}("");
