95d2ed3f68904433459c5ce62e9b8e56a3ded888
Aloysius Chan <aloycwl@gmail.com>
2022-04-27 15:38:47 +0800

    Update and rename OD_ERC721AC.sol to ERC721AC_TLD.sol

--- ERC721AC_TLD.sol
+++ ERC721AC_TLD.sol
@@ -2,7 +2,7 @@ pragma solidity^0.8.13;//SPDX-License-Identifier:None
 interface IERC721{event Transfer(address indexed from,address indexed to,uint256 indexed tokenId);event Approval(address indexed owner,address indexed approved,uint256 indexed tokenId);event ApprovalForAll(address indexed owner,address indexed operator,bool approved);function balanceOf(address owner)external view returns(uint256 balance);function ownerOf(uint256 tokenId)external view returns(address owner);function safeTransferFrom(address from,address to,uint256 tokenId)external;function transferFrom(address from,address to,uint256 tokenId)external;function approve(address to,uint256 tokenId)external;function getApproved(uint256 tokenId)external view returns(address operator);function setApprovalForAll(address operator,bool _approved)external;function isApprovedForAll(address owner,address operator)external view returns(bool);function safeTransferFrom(address from,address to,uint256 tokenId,bytes calldata data)external;}
 interface IERC721Metadata{function name()external view returns(string memory);function symbol()external view returns(string memory);function tokenURI(uint256 tokenId)external view returns(string memory);}
 interface IOwlWarLand{function MINT(address _t,uint256 _a)external;} 
-contract ERC721AC is IERC721,IERC721Metadata{
+contract ERC721AC_TeaLeafDefense is IERC721,IERC721Metadata{
     address private _owner;
     mapping(uint256=>address)private _tokenApprovals;
     struct Player{
