4c06797422e725f1ecffe30c8bfcec075ba9b829
"Aloy" <"aloycwl@gmail.com">
2022-05-23 10:12:28 +0800

    ERC20 virtual

--- ERC20AC_POT.sol
+++ ERC20AC_POT.sol
@@ -1,44 +1,20 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-contract ERC20AC_PotOfTea{
-    event Transfer(address indexed from,address indexed to,uint value);
-    event Approval(address indexed from,address indexed to,uint value);
-    mapping(address=>uint)private _balances;
+import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
+contract ERC20AC_PotOfTea is ERC20AC{
     mapping(address=>uint)private _access;
-    uint private _totalSupply;
     modifier onlyAccess(){require(_access[msg.sender]==1);_;}
     constructor(){_access[msg.sender]=1;}
-    function name()external pure returns(bytes32){return"Potty Of Tea";}
-    function symbol()external pure returns(bytes32){return"POT";}
-    function decimals()external pure returns(uint8){return 18;}
-    function totalSupply()external view returns(uint){return _totalSupply;}
-    function balanceOf(address a)external view returns(uint){return _balances[a];}
-    function transfer(address a,uint b)external returns(bool){
-        transferFrom(msg.sender,a,b);
-        return true;
-    }
-    function allowance(address a,address b)external pure returns(uint){
-        a;b;return 0;
-    }
-    function approve(address a,uint b)external pure returns(bool){
-        a;b;return true;
-    }
-    function transferFrom(address a,address b,uint c)public returns(bool){unchecked{
-        require(_balances[a]>=c&&(a==msg.sender||_access[msg.sender]==1));
-        (_balances[a]-=c,_balances[b]+=c);
-        emit Transfer(a,b,c);
-        return true;
-    }}
+    function name()external pure override returns(string memory){return"Pot Of Tea";}
+    function symbol()external pure override returns(string memory){return"POT";}
     function ACCESS(address a,uint b)external onlyAccess{
         if(b==0)delete _access[a];
         else _access[a]=1;
     }
     function MINT(address a,uint b)external onlyAccess{unchecked{
-        b*=1e18;
         (_totalSupply+=b,_balances[a]+=b);
         emit Transfer(address(0),a,b);
     }}
     function BURN(address a,uint b)external onlyAccess{unchecked{
-        b*=1e18;
         require(_balances[a]>=b);
         (_balances[a]-=b,_totalSupply-=b);
         emit Transfer(a,address(0),b);
