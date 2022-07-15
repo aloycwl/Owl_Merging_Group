56c163f0537ace49cdb87381632429e5b15ae50a
"Aloysiu" <"aloycwl@gmail.com">
2022-07-15 16:31:58 +0800

    Edited ERC20

--- ERC20AC_POT.sol
+++ ERC20AC_POT.sol
@@ -1,9 +1,8 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 import"https://github.com/aloycwl/ERC_AC/blob/main/ERC20AC/ERC20AC.sol";
 import"https://github.com/aloycwl/ERC_AC/blob/main/Util/OnlyAccess.sol";
-contract ERC20AC_PotOfTea is ERC20AC,OnlyAccess{
-    function name()external pure override returns(string memory){return"Pot Of Tea";}
-    function symbol()external pure override returns(string memory){return"POT";}
+contract POT is ERC20AC,OnlyAccess{
+    constructor(string memory name_,string memory sym_)ERC20AC(name_,sym_){}
     function MINT(address a,uint b)external onlyAccess{unchecked{
         (_totalSupply+=b,_balances[a]+=b);
         emit Transfer(address(0),a,b);
